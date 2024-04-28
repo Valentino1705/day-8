2b0384d3add47af77ce6e8a9958df07fa8402334
Felic Valentino <felicvalentino@Felic-MacBook-Pro.local>
2024-04-28 18:54:25 +0700

    edit day 8

--- testimonial.js
+++ testimonial.js
@@ -1,57 +1,68 @@
+
 class Testimonial {
-    image = "";
-    content = "";
-    author = "";
+  image = "";
+  content = "";
+  author = "";
 
-    constructor(image, content, author) {
-        this.image = image;
-        this.content = content;
-        this.author = author;
-    }
-    
-    html() {
-      throw new Error(
-        "You should use one of 'AuthorTestimonial' or 'CompanyTestimonial'"
-      );
-    }
-}
-
-class AuthorTestimonial extends Testimonial {
+  constructor(image, content, author) {
+      this.image = image;
+      this.content = content;
+      this.author = author;
+  }
+  
   html() {
-    return `<div class="testimonial">
-            <img src="${this.image}" class="profile-testimonial" />
-            <p class="quote">"${this.content}"</p>
-            <p class="author">- ${this.author}</p>
-        </div>`;
+    throw new Error(
+      "You should use one of 'AuthorTestimonial' or 'CompanyTestimonial'"
+    );
   }
 }
 
-const testimonial1  = new AuthorTestimonial (
-  "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=800",
-  "Mantap sekali jasnya",
-  "Felic Valentino"
-);
-
-const testimonial2  = new AuthorTestimonial (
-  "https://images.pexels.com/photos/733500/pexels-photo-733500.jpeg?auto=compress&cs=tinysrgb&w=800",
-  "Mikirin apa bro",
-  "Satrio Budi"
-);
-
-const testimonial3  = new AuthorTestimonial (
-  "https://images.pexels.com/photos/936119/pexels-photo-936119.jpeg?auto=compress&cs=tinysrgb&w=800",
-  "Apa yang buat kamu bahagia bro",
-  "Athena"
-);
-
-
-const testimonials = [testimonial1, testimonial2, testimonial3,];
-
-let testimonialsHTML = ``;
-
-for(let index = 0; index < testimonials.length; index++) {
-    testimonialsHTML += testimonials[index].html();  
+class AuthorTestimonial extends Testimonial {
+html() {
+  return `<div class="testimonial">
+          <img src="${this.image}" class="profile-testimonial" />
+          <p class="quote">"${this.content}"</p>
+          <p class="author">- ${this.author}</p>
+      </div>`;
+}
 }
 
-document.getElementById("testimonials").innerHTML = testimonialsHTML;
+//hof callback
+const datatestimonialBaru = [
+  {
+    image: "https://plus.unsplash.com/premium_photo-1713800447485-6bc73b9a1992?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8",
+    content: "capek bang?",
+    author: 'felic'
+  },
+  {
+    image: "https://plus.unsplash.com/premium_photo-1713800447485-6bc73b9a1992?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8",
+    content: "capek bang?",
+    author: 'felic'
+  },
+  {
+    image: "https://plus.unsplash.com/premium_photo-1713800447485-6bc73b9a1992?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8",
+    content: "capek bang?",
+    author: 'felic'
+  },
+  {
+    image: "https://plus.unsplash.com/premium_photo-1713800447485-6bc73b9a1992?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8",
+    content: "capek bang?",
+    author: 'felic'
+  },
+]
 
+const tambahTestimonial = (data, action) => {
+  action(data)
+}
+
+for(let index = 0; index < datatestimonialBaru.length; index++) {
+  tambahTestimonial(datatestimonialBaru[index], (data) => {
+    const testimonial  = new AuthorTestimonial (
+      data.image,
+      data.content,
+      data.author
+    );
+  
+    document.getElementById("testimonials").innerHTML += testimonial.html();
+  }) 
+}
